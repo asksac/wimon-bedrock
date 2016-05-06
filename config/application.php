94655b18429ee7058551169a7e6cf5a3ab25c4fc
Sachin Hamirwasia <github@asksac.com>
2016-05-06 12:11:04 -0500

    Disabled multisite in application.php

--- config/application.php
+++ config/application.php
@@ -83,6 +83,7 @@ if (!defined('ABSPATH')) {
 }
 
 /* Configure Multisite */
+/*
 define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false); // Set to true if using subdomain
@@ -90,3 +91,4 @@ define('DOMAIN_CURRENT_SITE', env('DOMAIN_CURRENT_SITE'));
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
+*/
