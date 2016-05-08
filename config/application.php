652c14136e6b42e181b517d7795c9804f792f86b
Sachin Hamirwasia <github@asksac.com>
2016-05-07 20:06:31 -0500

    Re-enabling multisite

--- config/application.php
+++ config/application.php
@@ -83,7 +83,6 @@ if (!defined('ABSPATH')) {
 }
 
 /* Configure Multisite */
-/*
 define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false); // Set to true if using subdomain
@@ -91,4 +90,3 @@ define('DOMAIN_CURRENT_SITE', env('DOMAIN_CURRENT_SITE'));
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
-*/
